54627fe6b1d3118059e648fa706129b47b285b66
paullee714 <ba93love@gmail.com>
2021-02-16 08:21:55 +0900

    basic user route

--- users/views.py
+++ users/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import ReadUserSerializer, RelatedUserSerializer
+from .models import User
+from rest_framework import status
 
 # Create your views here.
+
+class MeView(APIView):
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return Response(ReadUserSerializer(request.user).data)
+
+    def put(self, request):
+        pass
+
+
+@api_view(["GET"])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
