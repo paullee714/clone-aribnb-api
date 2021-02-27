f6bbd71eae4d1acb003189c114e9993baf8c96a6
Paul Lee <paul_lee@kakao.com>
2021-02-27 17:57:38 +0900

    user update logic

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ReadUserSerializer, RelatedUserSerializer
+from .serializers import ReadUserSerializer, RelatedUserSerializer, WriteUserSerializer
 from .models import User
 from rest_framework import status
 
@@ -9,18 +10,25 @@ from rest_framework import status
 
 class MeView(APIView):
 
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        if request.user.is_authenticated:
-            return Response(ReadUserSerializer(request.user).data)
+        return Response(ReadUserSerializer(request.user).data)
 
     def put(self, request):
-        pass
+        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET"])
 def user_detail(request, pk):
     try:
         user = User.objects.get(pk=pk)
+        return Response(ReadUserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
