73669ca290cdc49e79650f57fd30a26718020ee5
Paul Lee <paul_lee@kakao.com>
2021-02-28 22:36:45 +0900

    Create Account

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import ReadUserSerializer, RelatedUserSerializer, WriteUserSerializer
+from .serializers import UserSerializer
 from rooms.serializers import RoomSerializer
 from rooms.models import Room
 from .models import User
@@ -11,28 +11,29 @@ from rest_framework import status
 
 # Create your views here.
 
+class UsersView(APIView):
+    def post(self,request):
+        serializer = UserSerializer(request.data)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            return Response(UserSerializer(new_user))
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class MeView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        return Response(ReadUserSerializer(request.user).data)
+        return Response(UserSerializer(request.user).data)
 
     def put(self, request):
-        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response()
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(["GET"])
-def user_detail(request, pk):
-    try:
-        user = User.objects.get(pk=pk)
-        return Response(ReadUserSerializer(user).data)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class FavsView(APIView):
@@ -44,7 +45,7 @@ class FavsView(APIView):
         return Response(serializer)
 
     def post(self, request):
-        pk = request.data.get("pk",None)
+        pk = request.data.get("pk", None)
         user = request.user
         if pk is not None:
             try:
@@ -59,3 +60,12 @@ class FavsView(APIView):
 
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+def user_detail(request, pk):
+    try:
+        user = User.objects.get(pk=pk)
+        return Response(UserSerializer(user).data)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
