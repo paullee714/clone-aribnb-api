3766aa02d7001f7dac9f91c34f282186723e692d
Paul Lee <paul_lee@kakao.com>
2021-03-06 17:02:51 +0900

    update Pipfile and setting

--- config/settings.py
+++ config/settings.py
@@ -48,6 +48,7 @@ PROJECT_APPS = [
 
 THIRD_PARTY_APPS = [
     "rest_framework",
+    "jwt",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
@@ -143,13 +144,12 @@ AUTH_USER_MODEL = "users.User"
 
 #Django Rest Framework Config
 REST_FRAMEWORK = {
-    "DEFAULT_PAGINATION_CLASS" : 'rest_framework.pagination.PageNumberPagination',
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
-
-    "DEFAULT_AUTHENTICATION_CLASSES":[
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "config.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
-    ]
+    ],
 }
 
 if not DEBUG:
